a5f943a6719548882ccf0ea60370079dec68c5df
Max Stein <maxwell.stein@gmail.com>
2022-02-08 23:43:46 -0800

    imports audio-player-component

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,26 @@
 <template>
-  <div class="audio-player-container">
-    <audio ref="audioPlayer" crossorigin="anonymous" :src="src"></audio>
-    <div v-if="loading" class="loading">Loading...</div>
-    <div v-else-if="error" class="error">{{ error }}</div>
-    <AudioStreamPlayer v-else-if="isStream" :src="src" />
-    <AudioFilePlayer v-else :init-duration="duration" :audio-context="audioContext" />
+  <div v-if="src">
+    <AudioPlayer :src="src" :stream="stream" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import axios from 'axios'
-import AudioFilePlayer from './components/AudioFilePlayer.vue'
-import AudioStreamPlayer from './components/AudioStreamPlayer.vue'
+import AudioPlayer from 'audio-player'
 
 export default defineComponent({
-  components: { AudioFilePlayer, AudioStreamPlayer },
+  components: { AudioPlayer },
   data() {
     return {
-      src: null as string | null,
-      stream: null as string | null,
-      loading: true,
-      duration: 0,
-      audioContext: null as AudioContext | null,
-      error: null as string | null
-    }
-  },
-  computed: {
-    isStream(): any {
-      return this.stream !== null
+      src: undefined as string | undefined,
+      stream: undefined as string | undefined
     }
   },
   mounted() {
     this.src = this.$el.parentElement.getAttribute('src')
     this.stream = this.$el.parentElement.getAttribute('stream')
-    this.initAudioContext()
-  },
-  methods: {
-    async initAudioContext() {
-      if (!this.src) {
-        throw new Error('src is not set')
-      }
-      try {
-        if (this.isStream) {
-          const request = new XMLHttpRequest()
-          request.open('GET', this.src)
-          request.responseType = 'arraybuffer'
-          request.send()
-          request.onprogress = () => {
-            if (request.status === 200) {
-              request.abort()
-              this.loading = false
-            } else {
-              this.loading = false
-              console.error('stream not found')
-              this.error = 'Error'
-            }
-          }
-
-        } else {
-          if (!this.src) {
-            throw new Error('src is not set')
-          }
-          const { data } = await axios.get(this.src, {
-            responseType: 'arraybuffer'
-          })
-          this.audioContext = new AudioContext()
-          const { duration } = await this.audioContext.decodeAudioData(data)
-          this.duration = duration
-          this.loading = false
-        }
-      } catch (error: any) {
-        this.loading = false
-        console.log(error)
-        this.error = error.message
-      }
-    }
   }
 })
 </script>
 
-<style lang="scss">
-@import './assets/scss/main.scss';
-.audio-player-container {
-  font-size: 16px;
-  font-family: AuthenticSans, Arial, sans-serif;
-  .loading,
-  .error {
-    font-size: 1rem;
-    font-family: SpaceGrotesk, Arial, sans-serif;
-    padding: 1rem;
-  }
-  .audio-player {
-    font-family: SpaceGrotesk, Arial, sans-serif;
-    display: flex;
-    align-items: center;
-    // justify-content: center;
-    padding: 1rem;
-    .button {
-      height: 1rem;
-      width: 1rem;
-      flex-shrink: 0;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
--- vite.config-component.ts
+++ vite.config-component.ts
@@ -9,9 +9,21 @@ export default defineConfig({
   plugins: [vue(), svgLoader(), dts()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/main.ts'),
+      entry: path.resolve(__dirname, 'src/App.vue'),
       name: 'AudioPlayer',
       fileName: format => `audio-player.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
     }
   }
 })
